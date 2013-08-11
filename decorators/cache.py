65668a775520c8ffc987f46c67281d53b3532ebf
Adam Hill <adamghill@gmail.com>
2013-08-11 08:54:47 -0400

    Initial commit.

---
+++ decorators/cache.py
@@ -0,0 +1,46 @@
+from django.core.cache import cache
+import cPickle as pickle
+import hashlib
+
+
+# This will set a temporary value while ``func`` is being
+# processed. When using threads, this is vital, as otherwise
+# the function can be called several times before it finishes
+# and is put into the cache.
+class MethodNotFinishedError(Exception):
+    pass
+
+
+def cache_function(length):
+    """
+    A variant of the snippet posted by Jeff Wheeler at
+    http://www.djangosnippets.org/snippets/109/
+
+    Caches a function, using the function and its arguments as the key, and the return
+    value as the value saved. It passes all arguments on to the function, as
+    it should.
+
+    The decorator itself takes a length argument, which is the number of
+    seconds the cache will keep the result around.
+
+    It will put in a MethodNotFinishedError in the cache while the function is
+    processing. This should not matter in most cases, but if the app is using
+    threads, you won't be able to get the previous value, and will need to
+    wait until the function finishes. If this is not desired behavior, you can
+    remove the first two lines after the ``else``.
+    """
+    def decorator(func):
+        def inner_func(*args, **kwargs):
+            raw = [func.__name__, func.__module__, args, kwargs]
+            pickled = pickle.dumps(raw, protocol=pickle.HIGHEST_PROTOCOL)
+            key = hashlib.md5(pickled).hexdigest()
+            value = cache.get(key)
+
+            if key in cache:
+                return value
+            else:
+                result = func(*args, **kwargs)
+                cache.set(key, result, length)
+                return result
+        return inner_func
+    return decorator
---
+++ query_dict.py
@@ -0,0 +1,17 @@
+def convert_to_querystring(query_dict, clean=True):
+    '''
+    Convert QueryDict into querystring. Useful when doing the GET-POST-REDIRECT pattern.
+    Cleans --Select-- and blank values from the querystring by default.
+    '''
+    query_dict_copy = query_dict.copy()
+
+    for query in query_dict:
+        if query == 'csrfmiddlewaretoken':
+            del query_dict_copy[query]
+        elif clean and query_dict_copy[query] == '--Select--':
+            del query_dict_copy[query]
+        elif clean and not query_dict_copy[query]:
+            del query_dict_copy[query]
+
+    querystring = query_dict_copy.urlencode(True)
+    return querystring
