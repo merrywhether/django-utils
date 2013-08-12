ee39296cc3c6613915dbb43e5ef60cc363efa7aa
Adam Hill <adamghill@gmail.com>
2013-08-11 20:18:30 -0400

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+version = '0.0.1.0'
+install_requires = []
+dependency_links = []
+
+setup(name='django-utils',
+      version = version,
+      description = 'Django utilities',
+      author = "The Motley Fool",
+      author_email = "github@fool.com",
+      url = "http://www.fool.com/",
+      packages = find_packages(),
+      include_package_data = True,
+      install_requires = install_requires,
+      dependency_links = dependency_links,
+      zip_safe = False,
+      classifiers=[
+          'Intended Audience :: Developers',
+          'Operating System :: OS Independent',
+          'Topic :: Software Development'
+    ],
+)
